7bd3ec94b6438225c95fc7f3e00bc5e609e13fe3
wastecross <charly_1310@outlook.com>
2020-09-09 14:40:42 -0500

    feat: add input
    - Add test for input.
    - Add input component.

---
+++ base-for-hooks-context/src/Input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          onClick={(evt) => {
+            evt.preventDefault();
+          }}
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
+
+export default Input;
